08bf69b29aaa74a982bc1ac8b67aacdf0cb04def
rajatjhamnani <rajatjhamnani@gmail.com>
2024-03-05 00:46:21 +0530

    Final touch to About US page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,4 +1,19 @@
+import Link from "next/link";
 const AboutUsPage = () => {
-  return <h1>This is my About Us page</h1>;
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+  return (
+    <>
+      <h1>This is my About Us page</h1>
+      {details.map((data, idx) => (
+        <Link key={data.id} href={`/aboutus/${idx}`}>
+          <h1>{data.name}</h1>
+        </Link>
+      ))}
+    </>
+  );
 };
 export default AboutUsPage;
