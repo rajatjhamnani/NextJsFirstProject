9f47afad9738cca5197f96bfdbd373c615a59fcf
rajatjhamnani <rajatjhamnani@gmail.com>
2024-03-04 18:22:41 +0530

    Why do we need to learn Next JS

---
+++ pages/aboutus.js
@@ -0,0 +1,4 @@
+const AboutUsPage = () => {
+  return <h1>This is my About Us page</h1>;
+};
+export default AboutUsPage;
---
+++ pages/news.js
@@ -0,0 +1,4 @@
+const NewsPage = () => {
+  return <h1>This is my News page</h1>;
+};
+export default NewsPage;
